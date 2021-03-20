17c2f352f81ae685cb4d90be4d4275f302761c7b
MA Jian <majian1979@qq.com>
2021-03-20 10:47:45 +0800

    Update copent.R

--- R/copent.R
+++ R/copent.R
@@ -1,18 +1,21 @@
 ##################################################################################
-###  Estimating Copula Entropy from data
-###  2019-07-03
+###  Estimating Copula Entropy and Transfer Entropy
+###  2021-03-20
 ###  by MA Jian (Email: majian03@gmail.com)
 ###
 ###  Parameters
 ###   x		: N * d data, N samples, d dimensions
 ###   k 	: kth nearest neighbour, parameter for kNN entropy estimation 
 ###   dt	: distance type [1: 'Euclidean', others: 'Maximum distance']
+###   lag	: time lag
 ###
 ###  References
 ###  [1] Ma Jian, Sun Zengqi. Mutual information is copula entropy. 
 ###      arXiv:0808.0845, 2008.
 ###  [2] Kraskov A, St\"ogbauer H, Grassberger P. Estimating mutual information. 
 ###      Physical review E, 2004, 69(6): 066138.
+###  [3] Ma Jian. Estimating Transfer Entropy via Copula Entropy. 
+###      arXiv preprint arXiv:1910.04375, 2019.
 ##################################################################################
 copent<-function(x,k=3,dt=2){
   xc = construct_empirical_copula(x)
@@ -56,4 +59,28 @@ entknn<-function(x,k=3,dt=2){
   }
   
   g1 + log(cd) + logd
+}
+
+ci<-function(x,y,z,k=3,dt=2){
+  xyz = cbind(x,y,z)
+  xz  = cbind(x,z)
+  yz  = cbind(y,z)
+  
+  copent(xyz,k,dt) - copent(xz,k,dt) - copent(yz,k,dt)
+}
+
+transent<-function(x,y,lag=1,k=3,dt=2){
+  xl = length(x)
+  yl = length(y)
+  if (xl > yl){
+    l = yl
+  }
+  else {
+    l = xl
+  }
+  x1 = x[1:(l-lag)]
+  x2 = x[(lag+1):l]
+  y1 = y[1:(l-lag)]
+
+  ci(x2,y1,x1,k,dt) 
 }
